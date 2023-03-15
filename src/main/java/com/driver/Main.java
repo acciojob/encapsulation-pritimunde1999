0ab74bf9f65732a662ad31ea0c1886d9452a6d0d
pritimunde1999 <pritimunde0109@gmail.com>
2023-03-15 20:16:05 +0530

    Assignment done

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,21 @@
 package com.driver;
 
+import com.sun.jdi.Field;
+
 public class Main {
-  
+
+    public static void main(String[]args) {
+        RWOnly rw = new RWOnly();
+
+        //rw.name = "priti";
+
+        //'name' has private access in 'com.driver.RWOnly'
+
+        rw.setName("Priti");
+
+        System.out.println(rw.getName());
+
+    }
+
+
 }
